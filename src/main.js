f2f74c46175c2527fb3900a8746d7e3c844aec55
Icaro Oliveira <i.oliveira@coblue.com.br>
2025-01-20 00:39:06 -0300

    feat(theme): define custom color preset for PrimeVue theme

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import i18n from './i18n';
 
 import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
@@ -13,10 +14,29 @@ import '@/assets/tailwind.css';
 
 const app = createApp(App);
 
+// Documentation to alter project's primary color  https://primevue.org/theming/styled/#noir
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{teal.50}',
+            100: '{teal.100}',
+            200: '{teal.200}',
+            300: '{teal.300}',
+            400: '{teal.400}',
+            500: '{teal.500}',
+            600: '{teal.600}',
+            700: '{teal.700}',
+            800: '{teal.800}',
+            900: '{teal.900}',
+            950: '{teal.950}'
+        }
+    }
+});
+
 app.use(router);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
             darkModeSelector: '.app-dark'
         }
