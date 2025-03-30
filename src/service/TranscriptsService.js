f3d3fc89d2b8a9e605ebb9c662791e3fe6706ac8
Icaro Oliveira <i.oliveira@coblue.com.br>
2025-03-30 17:10:27 -0300

    feat(auth): store access token in cookies after login and update transcripts service to use cookie token

--- src/service/TranscriptsService.js
+++ src/service/TranscriptsService.js
@@ -1,13 +1,13 @@
 import api from '@/services/axios';
+import Cookies from 'js-cookie';
 
 export const TranscriptsService = {
-
-
     async index() {
+        const token = Cookies.get('token');
         try {
             const response = await api.get('/user/transcripts', {
                 headers: {
-                    Authorization: `Bearer ${localStorage.getItem('access_token')}`
+                    Authorization: `Bearer ${token}`
                 }
             });
 
--- src/views/pages/auth/api.js
+++ src/views/pages/auth/api.js
@@ -1,4 +1,5 @@
 import api from '@/services/axios';
+import Cookies from 'js-cookie';
 
 export const register = async (payload) => {
     try {
@@ -12,6 +13,9 @@ export const register = async (payload) => {
 export const login = async (payload) => {
     try {
         const response = await api.post('/login', payload);
+
+        Cookies.set('token', response.data.access_token);
+
         return response.data;
     } catch (error) {
         return Promise.reject(error);
